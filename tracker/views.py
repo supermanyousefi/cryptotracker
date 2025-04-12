d78c23a24acdbfecd5f01b7dcfe418845f44d8ea
supermanyousefi <supermanyousefi@gmail.com>
2025-04-12 11:20:11 +0330

    Sprint 3: Templates + Base Layout

--- tracker/views.py
+++ tracker/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Welcome to CryptoTracker 🪙</h1>")
+    return render(request,"tracker/home.html")
