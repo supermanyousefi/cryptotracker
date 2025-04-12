cd8a051ee07f94772d8985f76fdeb953cef73c58
supermanyousefi <supermanyousefi@gmail.com>
2025-04-12 14:13:19 +0330

    Sprint 5: Integrate Real-Time Price API (Crypto & Currency)

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 # Create your views here.
 def home(request):
-    return render(request,"tracker/home.html")
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
+    response = requests.get(url)
+    data = response.json()
+    btc_price = data['bitcoin']['usd']
+    context = {'btc_price':btc_price}
+    return render(request,'tracker/home.html',context)
